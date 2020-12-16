e3f5b6d97b334d49f84c99c0c3a7e8c85da21462
Aman Pandey <58739585+stark004@users.noreply.github.com>
2020-12-17 04:11:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "💌": "Love Letter",
+  "💣": "Bomb",
+  "🛌": "Person in Bed",
+  "🗺️": "World Map",
+  "🧭": "Compass",
+  "🧱": "Brick",
+  "💈": "Barber Pole",
+  "🦽": "Wheelchair",
+  "🛢️": "Oil Drum",
+  "🛎️": "Bellhop Bell",
+  "🧳": "Luggage",
+  "⌛": "Hourglass",
+  "⌚": "Watch",
+  "⏱️": "Stopwatch"
+};
+
+const emojiList = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [input, setInput] = useState("");
+
+  function inputHandler(e) {
+    let userInput = e.target.value;
+    if (emojiDictionary[userInput] === undefined) {
+      emojiDictionary[userInput] =
+        "We don't have this in our database. Please try something else.";
+    }
+    setInput(emojiDictionary[userInput]);
+  }
+
+  function clickHandler(emoji) {
+    setInput(emojiDictionary[emoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Miscellaneous Object Emojis</h1>
+      <input className="input" onChange={inputHandler} />
+      <h3>{input}</h3>
+      <h2>Emoji List</h2>
+      <div>
+        {emojiList.map((emoji) => {
+          return (
+            <span
+              onClick={() => clickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.4rem", cursor: "pointer" }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
